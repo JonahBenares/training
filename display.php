057f5a7d9f79d39a4b4b84a1d2ab30c463860e9a
hennetanan <hentanan.cenpri@gmail.com>
2022-08-04 15:32:58 +0800

    complete displaying and include age

--- display.php
+++ display.php
@@ -19,17 +19,23 @@
 		<td></td>
 	</tr>
 	<?php while($fetch = $sql->fetch_array()){ ?>
+		<?php 
+			$bday=$fetch['birthday'];
+			$query = $conn->query("SELECT FROM employees WHERE birthday = '$bday'");
+			$currentDate = date("d-m-Y");
+			$age = date_diff(date_create($bday), date_create($currentDate));	
+		?>
 		<tr>
 			<td><?php echo $fetch['lname'] .", ". $fetch['fname'] ." ". $fetch['mname']; ?></td>
 			<td><?php echo $fetch['birthday']; ?></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
+			<td><?php echo $age->format("%y");?></td>
+			<td><?php echo $fetch['position']; ?></td>
+			<td><?php echo $fetch['company']; ?></td>
+			<td><?php echo $fetch['address']; ?></td>
+			<td><?php echo $fetch['phone_no']; ?></td>
 			<td><a href="update.php?id=<?php echo $fetch['employee_id']; ?>">[Edit]</a>
 				<a href="delete.php?id=<?php echo $fetch['employee_id']; ?>">[Delete]</a></td>
 		</tr>
 	<?php } ?>
 </table>
-	
+		
