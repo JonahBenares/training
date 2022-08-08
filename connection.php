d83f92031bfecc9d59da77b5882fcba2d4112d67
henntanan <hentanan.cenpri@gmail.com>
2022-08-08 14:26:18 +0800

    add enrollment_system

--- connection.php
+++ connection.php
@@ -1,15 +1,14 @@
 <?php
-  $servername = "localhost";
-  $username = "root";
-  $password = "";
-  $db = "db_enrollment";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "db_training";
 
-  // Create connection
-  $conn = new mysqli($servername, $username, $password, $db);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
 
-  // Check connection
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
-  // echo "Connected successfully";
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
 ?>
